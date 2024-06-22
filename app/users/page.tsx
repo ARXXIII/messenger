f8df6defeccba7e419e5b542e62b6da9a9019d52
Андрей Романов <human1488pwnz@yandex.ru>
2024-06-22 18:56:39 +0300

    Chat window

---
+++ app/users/page.tsx
@@ -0,0 +1,11 @@
+import { Empty } from "../components"
+
+const Users = () => {
+    return (
+        <div className="hidden lg:block lg:pl-80 h-full">
+            <Empty />
+        </div>
+    )
+}
+
+export default Users
