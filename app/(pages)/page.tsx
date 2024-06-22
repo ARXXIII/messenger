01d2ed94f274a802a2f68fe02b4d021006545b58
Андрей Романов <human1488pwnz@yandex.ru>
2024-06-22 18:54:03 +0300

    Little redesign

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -2,9 +2,9 @@ import { AuthForm } from "./components";
 
 export default function Home() {
     return (
-        <article className="flex flex-col min-h-screen justify-center items-center p-24">
+        <article className="flex flex-col justify-center items-center lg:p-24 h-full">
             <section>
-                <h1 className="font-bold text-4xl tracking-wide">Sign up to begin</h1>
+                <h1 className="font-bold text-3xl text-neutral-100 tracking-wide">AR23 | Messenger</h1>
             </section>
             <section>
                 <AuthForm />
