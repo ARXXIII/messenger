adea629b8a94414d736ad0ad1965da3a92dc28c9
Андрей Романов <human1488pwnz@yandex.ru>
2024-06-22 18:50:47 +0300

    Global components

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,4 +1,21 @@
+import Empty from './Empty';
 import Button from './Button';
+import Avatar from './Avatar';
 import Input from './inputs/Input';
+import Sidebar from './sidebar/Sidebar';
+import MobileItem from './sidebar/MobileItem';
+import DesktopItem from './sidebar/DesktopItem';
+import MobileFooter from './sidebar/MobileFooter';
+import DesktopSidebar from './sidebar/DesktopSidebar';
 
-export { Button, Input };
+export {
+	Input,
+	Empty,
+	Button,
+	Avatar,
+	Sidebar,
+	MobileItem,
+	DesktopItem,
+	MobileFooter,
+	DesktopSidebar,
+};
