7f78d1f7425e99e00d056fb310261f8fea2af0c7
Андрей Романов <human1488pwnz@yandex.ru>
2024-06-22 18:49:16 +0300

    Added shadcn/ui

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import clsx from "clsx"
+import { cn } from "@/lib/utils"
 import { ButtonProps } from "../types"
 
 const Button = ({
@@ -17,12 +17,12 @@ const Button = ({
             type={type}
             disabled={disabled}
             onClick={onClick}
-            className={clsx('flex justify-center items-center p-3 font-bold text-sm rounded-xl',
+            className={cn('flex justify-center items-center p-3 font-bold text-sm rounded-xl',
                 disabled && 'opacity-50 cursor-default',
                 fullWidth && 'w-full',
                 secondary ? 'text-gray-900' : 'text-white',
                 danger && 'bg-red-500 hover:bg-rose-600',
-                !secondary && !danger && 'bg-sky-500 hover:bg-sky-600'
+                !secondary && !danger && 'bg-purple-700 hover:bg-purple-800'
             )}
         >
             {children}
--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -1,8 +1,7 @@
 'use client'
 
-import clsx from "clsx"
+import { cn } from "@/lib/utils"
 import { InputProps } from "@/app/types"
-import { FieldError, FieldValues, UseFormRegister } from "react-hook-form"
 
 const Input = ({
     id,
@@ -15,7 +14,7 @@ const Input = ({
 }: InputProps) => {
     return (
         <div>
-            <label htmlFor={id} className="block font-medium text-sm">{label}</label>
+            <label htmlFor={id} className="block font-medium text-sm text-white">{label}</label>
             <div className="mt-3">
                 <input
                     id={id}
@@ -23,7 +22,7 @@ const Input = ({
                     autoComplete={id}
                     disabled={disabled}
                     {...register(id, { required })}
-                    className={clsx(`p-3 w-full lg:min-w-[300px] border-2 border-gray-200 focus:border-green-500 rounded-xl shadow duration-200 ease-in`,
+                    className={cn(`p-3 w-full lg:min-w-[300px] text-white border-2 border-zinc-800 focus:border-purple-500 bg-transparent rounded-xl duration-200 ease-in`,
                         errors[id] && 'border-rose-500',
                         disabled && 'opacity-50 cursor-default'
                     )}
