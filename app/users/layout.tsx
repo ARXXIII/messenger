3c2bad790b3452e6b53cdc67ff90e7fe8e0e372c
Андрей Романов <human1488pwnz@yandex.ru>
2024-06-23 17:09:31 +0300

    Users layout

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,13 +1,19 @@
+import getUsers from "../actions/getUsers";
+
 import { Sidebar } from "../components";
+import { UserList } from "./components";
 
 export default async function UsersLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const users = await getUsers()
+
     return (
         <Sidebar>
-            <div className="h-full">
+            <div className="flex justify-between items-center gap-12 h-full">
+                <UserList users={users} />
                 {children}
             </div>
         </Sidebar>
