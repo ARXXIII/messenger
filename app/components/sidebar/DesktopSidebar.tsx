3316f17adb0b719813b3139d416295996061e0ce
Андрей Романов <human1488pwnz@yandex.ru>
2024-06-24 18:07:19 +0300

    DesktopSidebar style changes

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -1,20 +1,21 @@
 'use client'
 
-import { DesktopSidebarProps } from "@/app/types"
 import Avatar from "../Avatar"
 import DesktopItem from "./DesktopItem"
 import useRoutes from "@/app/hooks/useRoutes"
 
 import { useState } from "react"
+import { DesktopSidebarProps } from "@/app/types"
 
 const DesktopSidebar = ({ currentUser }: DesktopSidebarProps) => {
     const routes = useRoutes()
     const [isOpen, setIsOpen] = useState<boolean>(false)
 
     return (
-        <div className="hidden lg:flex lg:flex-col justify-between lg:p-3 lg:inset-y-0 lg:z-40 lg:bg-zinc-900 lg:overflow-y-auto lg:rounded-xl">
+        <div className="hidden lg:flex lg:flex-col justify-between lg:min-w-[110px] lg:p-3 lg:inset-y-0 lg:z-40 lg:bg-zinc-900 lg:overflow-y-auto lg:rounded-xl">
             <nav className="flex flex-col justify-between">
                 <ul role="list" className="grid grid-cols-1 items-center gap-y-3">
+
                     {routes.map((item) => (
                         <DesktopItem
                             key={item.label}
@@ -25,6 +26,7 @@ const DesktopSidebar = ({ currentUser }: DesktopSidebarProps) => {
                             onClick={item.onClick}
                         />
                     ))}
+
                 </ul>
             </nav>
             <nav className="flex flex-col justify-between items-center">
