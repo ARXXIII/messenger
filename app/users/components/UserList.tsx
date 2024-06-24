1693670f199d1a53183974f91f0babcb767eba6a
Андрей Романов <human1488pwnz@yandex.ru>
2024-06-24 18:08:42 +0300

    UserList style changes

--- app/users/components/UserList.tsx
+++ app/users/components/UserList.tsx
@@ -1,20 +1,23 @@
 'use client'
 
-import { UserListProps } from "@/app/types"
 import UserBox from "./UserBox"
+
+import { UserListProps } from "@/app/types"
 
 const UserList = ({ users }: UserListProps) => {
     return (
-        <aside className="inset-y-0 w-1/4 h-full">
+        <aside className="inset-y-0 w-full lg:w-1/4 h-full">
             <div>
                 <div className="flex-col">
-                    <div className="mb-6 font-bold text-neutral-100 text-2xl">
+                    <div className="mb-6 font-bold text-2xl text-neutral-100 tracking-wide">
                         People
                     </div>
                 </div>
+
                 {users.map((user) => (
                     <UserBox key={user.id} data={user} />
                 ))}
+
             </div>
         </aside>
     )
