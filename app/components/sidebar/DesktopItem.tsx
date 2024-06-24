9ce87770df5a3fcd9100fa045f6a26023ac944e9
Андрей Романов <human1488pwnz@yandex.ru>
2024-06-24 18:07:04 +0300

    DesktopItem style changes

--- app/components/sidebar/DesktopItem.tsx
+++ app/components/sidebar/DesktopItem.tsx
@@ -14,8 +14,8 @@ const DesktopItem = ({ href, label, icon: Icon, active, onClick }: DesktopItemPr
 
     return (
         <li onClick={handleClick}>
-            <Link href={href} className={cn(`group flex flex-col items-center gap-y-3 p-3 text-sm text-neutral-300 hover:text-white hover:bg-zinc-800/50 leading-relaxed rounded-xl`,
-                active && 'text-purple-700 hover:text-purple-600 bg-zinc-800 hover:bg-zinc-800'
+            <Link href={href} className={cn(`group flex flex-col items-center gap-y-3 p-3 text-center text-sm text-neutral-300 hover:text-white hover:bg-zinc-800/50 leading-relaxed rounded-xl`,
+                active && 'text-purple-700 hover:text-purple-600 bg-zinc-800 hover:bg-zinc-800 shrink-0'
             )}>
                 <Icon className='w-6 h-6 shrink-0' />
                 {label}
