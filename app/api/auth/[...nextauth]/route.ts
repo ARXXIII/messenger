502ced834900db5dd8e18359ebddfb1362991ad7
Андрей Романов <human1488pwnz@yandex.ru>
2024-06-27 18:19:42 +0300

    [fix] Type Error: 'AuthOptions' is not assignable to type 'never'

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,63 +1,6 @@
-import bcrypt from 'bcrypt';
-import prisma from '@/app/libs/prismadb';
-import Github from 'next-auth/providers/github';
-import Google from 'next-auth/providers/google';
-import NextAuth, { AuthOptions } from 'next-auth';
-import Credentials from 'next-auth/providers/credentials';
+import NextAuth from 'next-auth';
 
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-
-export const authOptions: AuthOptions = {
-	adapter: PrismaAdapter(prisma),
-	providers: [
-		Github({
-			clientId: process.env.GITHUB_ID as string,
-			clientSecret: process.env.GITHUB_SECRET as string,
-		}),
-		Google({
-			clientId: process.env.GOOGLE_CLIENT_ID as string,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-		}),
-		Credentials({
-			name: 'credentials',
-			credentials: {
-				email: { label: 'email', type: 'text' },
-				password: { label: 'password', type: 'password' },
-			},
-			async authorize(credentials) {
-				if (!credentials?.email || !credentials?.password) {
-					throw new Error('Invalid credentials');
-				}
-
-				const user = await prisma.user.findUnique({
-					where: {
-						email: credentials.email,
-					},
-				});
-
-				if (!user || !user?.hashedPassword) {
-					throw new Error('Invalid credentials');
-				}
-
-				const isCorrectPassword = await bcrypt.compare(
-					credentials.password,
-					user.hashedPassword
-				);
-
-				if (!isCorrectPassword) {
-					throw new Error('Invalid credentials');
-				}
-
-				return user;
-			},
-		}),
-	],
-	debug: process.env.NODE_ENV === 'development',
-	session: {
-		strategy: 'jwt',
-	},
-	secret: process.env.NEXTAUTH_SECRET,
-};
+import { authOptions } from '@/app/utils/authOptions';
 
 const handler = NextAuth(authOptions);
 
