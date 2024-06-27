fe73168fd705501726357493efec7e02034eb446
Андрей Романов <human1488pwnz@yandex.ru>
2024-06-27 17:01:08 +0300

    Button changes
    Changed text color for secondary variant and added background-color
    Changed background-color for danger variant

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -17,12 +17,12 @@ const Button = ({
             type={type}
             disabled={disabled}
             onClick={onClick}
-            className={cn('flex justify-center items-center p-3 font-bold text-sm rounded-xl',
+            className={cn('flex justify-center items-center p-3 font-bold text-sm tracking-wide rounded-lg transition',
                 disabled && 'opacity-50 cursor-default',
                 fullWidth && 'w-full',
-                secondary ? 'text-gray-900' : 'text-white',
-                danger && 'bg-red-500 hover:bg-rose-600',
-                !secondary && !danger && 'bg-purple-700 hover:bg-purple-800'
+                secondary ? 'text-neutral-300 bg-zinc-800' : 'text-white',
+                danger && 'bg-rose-500 hover:bg-rose-600',
+                !secondary && !danger && 'bg-purple-700 hover:bg-purple-600'
             )}
         >
             {children}
