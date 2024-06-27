5f80238f720c0b95cbfbab46214dbe7d3c723da3
Андрей Романов <human1488pwnz@yandex.ru>
2024-06-27 17:01:49 +0300

    Empty changes
    Added w-full class

--- app/components/Empty.tsx
+++ app/components/Empty.tsx
@@ -1,6 +1,6 @@
 const Empty = () => {
     return (
-        <div className="flex justify-center items-center p-6 h-full bg-zinc-900 lg:rounded-xl">
+        <div className="flex justify-center items-center p-6 w-full h-full bg-zinc-900 rounded-xl">
             <div className="flex flex-col items-center text-center">
                 <h3 className="px-3 py-1 font-semibold text-neutral-300 tracking-wide rounded-full bg-zinc-800">Select a chat or start messaging</h3>
             </div>
