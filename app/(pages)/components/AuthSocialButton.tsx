1b1cbc7a7645af892490d2fb6d29604f9c1de6ef
Андрей Романов <human1488pwnz@yandex.ru>
2024-06-27 16:56:13 +0300

    AuthSocialButton changes
    Changed text color
    Added hover effect
    Changed border radius

--- app/(pages)/components/AuthSocialButton.tsx
+++ app/(pages)/components/AuthSocialButton.tsx
@@ -1,12 +1,11 @@
 import { AuthSocialButtonProps } from "@/app/types"
-import { IconType } from "react-icons"
 
 const AuthSocialButton = ({ icon: Icon, onClick }: AuthSocialButtonProps) => {
     return (
         <button
             type="button"
             onClick={onClick}
-            className="flex justify-center p-3 w-full text-gray-400 bg-zinc-800 hover:bg-gray-50 rounded-xl"
+            className="flex justify-center p-3 w-full text-gray-500 bg-zinc-800 hover:bg-zinc-700/50 rounded-lg transition"
         >
             <Icon />
         </button>
