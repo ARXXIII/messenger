a176b8706815ada6ecdb5dc85a4dea1c99d07b8a
Андрей Романов <human1488pwnz@yandex.ru>
2024-06-27 16:59:01 +0300

    Home page changes
    Added grid
    
    #Logo changes
    Changed font-weight from bold to black
    Added font-size for mobiles
    Centered text

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -2,13 +2,15 @@ import { AuthForm } from "./components";
 
 export default function Home() {
     return (
-        <article className="flex flex-col justify-center items-center lg:p-24 h-full">
-            <section>
-                <h1 className="font-bold text-3xl text-neutral-100 tracking-wide">AR23 | Messenger</h1>
-            </section>
-            <section>
-                <AuthForm />
-            </section>
+        <article className="flex justify-center items-center lg:p-24 h-full">
+            <div className="grid grid-cols-1 gap-y-6">
+                <section>
+                    <h1 className="text-center p-6 font-black text-2xl sm:text-3xl text-neutral-100 tracking-wide bg-zinc-900 rounded-xl shadow">AR23 | GigaMessenger</h1>
+                </section>
+                <section>
+                    <AuthForm />
+                </section>
+            </div>
         </article>
     );
 }
